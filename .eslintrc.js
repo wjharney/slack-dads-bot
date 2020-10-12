372a75d9558f2db619687ade42024014f3189113
Will Harney <41450688+wjharney@users.noreply.github.com>
2020-10-11 20:16:12 -0400

    Use standard-with-typescript isntead of standard.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: false,
     es2021: true
   },
-  extends: ['standard'],
+  extends: ['standard-with-typescript'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
+    project: 'tsconfig.json'
   },
   plugins: ['@typescript-eslint'],
   reportUnusedDisableDirectives: true,
