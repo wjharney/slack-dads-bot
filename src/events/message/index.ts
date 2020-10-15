08f2b0ea1baf4393370b6d1ea92bd669941444f7
Will Harney <41450688+wjharney@users.noreply.github.com>
2020-10-14 21:42:53 -0400

    Implement message handler.

--- src/events/message/index.ts
+++ src/events/message/index.ts
@@ -1,1 +1,34 @@
-export const onMessage = async (): Promise<void> => { }
+import { Action, ActionAttributes } from '../../db'
+
+interface MessageEvent {
+  type: 'message'
+  subtype: string
+  channel_type: 'channel'
+  user: string
+  ts: string
+  team: string
+}
+
+const shouldHandle = (event: MessageEvent): boolean => {
+  return event.channel_type === 'channel' && event.subtype !== 'bot_message'
+}
+
+/**
+ * Handle a Slack message event
+ * @param event Slack event object
+ * @returns Boolean indicating whether the event was handles
+ */
+export const onMessage = async (event: MessageEvent): Promise<boolean> => {
+  if (!shouldHandle(event)) {
+    return false // Don't handle this event
+  }
+  const parsed: ActionAttributes = {
+    action: 'message',
+    team: event.team,
+    user: event.user,
+    timestamp: new Date(+event.ts * 1000)
+  }
+  console.log(`[${parsed.timestamp.toISOString()}] ${parsed.action}: ${parsed.team}/${parsed.user}`)
+  await Action.upsert(parsed)
+  return true
+}
