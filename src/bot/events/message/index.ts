50936facb6758519e182391075a84e693ed4896d
Will Harney <41450688+wjharney@users.noreply.github.com>
2020-12-09 21:38:14 -0500

    Catch events with missing data.

--- src/bot/events/message/index.ts
+++ src/bot/events/message/index.ts
@@ -5,13 +5,33 @@ const shouldHandle = (event: MessageEvent): boolean => {
   return typeof event.channel === 'string' && !event.subtype
 }
 
+const validBody = (body: SlackEventMiddlewareArgs<'message'>['body']): boolean => {
+  let valid = true
+  if (!body.team_id) {
+    valid = false
+    console.error('Message body is missing "team_id"')
+  }
+  if (!body.event.user) {
+    valid = false
+    console.error('Message body is missing "event.user"')
+  }
+  if (!body.event.ts) {
+    valid = false
+    console.error('Message body is missing "event.ts"')
+  }
+  if (!valid) {
+    console.log(body)
+  }
+  return valid
+}
+
 /**
  * Handle a Slack message event
  * @param event Slack event object
  * @returns Boolean indicating whether the event was handles
  */
 export const onMessage = async ({ body, event }: SlackEventMiddlewareArgs<'message'>): Promise<void> => {
-  if (!shouldHandle(event)) {
+  if (!shouldHandle(event) || !validBody(body)) {
     return // Don't handle this event
   }
   const parsed: ActionAttributes = {
